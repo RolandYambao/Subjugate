5692e6278177a7263756178078bb6a57eec3ca09
RolandYambao <rolandyambao@gmail.com>
2021-12-07 15:38:57 -0800

    Updated

--- controllers/factions.js
+++ controllers/factions.js
@@ -14,24 +14,4 @@ router.get('/', function (req, res) {
         });
 });
 
-// Post Route
-router.get('/', function (req, res) {
-    Faction.create({
-        firstFaction: req.body.firstFaction,
-        secondFaction: req.body.secondFaction,
-        thirdFaction: req.body.thirdFaction,
-    })
-        .then(function (newFaction) {
-            newFaction = newFaction.toJSON();
-            res.redirect('/artists/ ' + newArtist.id);
-        })
-        .catch(function (error) {
-            console.log('ERROR', error);
-            res.render('404', { message: 'Artist was not added, please try again' });
-        });
-    // res.redirect()
-});
-
-// Post Route
-
 module.exports = router;
--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const passport = require('./config/ppConfig');
 const isLoggedIn = require('./middleware/isLoggedIn');
 
 const {
-  Factions,
-  FactionTraits,
-  FactionUnits,
-  FactionBuildings,
-  FactionPowers,
-  GroundUnits,
-  NavalUnits,
-  AirUnits,
-  BaseBuildings,
-  ProductionBuildings,
-  DefensiveBuildings,
-  OffensivePowers,
-  DefensivePowers,
-  StatusPowers,
+  factions,
+  factionTraits,
+  factionUnits,
+  factionBuildings,
+  factionPowers,
+  groundUnits,
+  navalUnits,
+  airUnits,
+  baseBuildings,
+  productionBuildings,
+  defensiveBuildings,
+  offensivePowers,
+  defensivePowers,
+  statusPowers,
 } = require("./models");
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
@@ -38,6 +38,9 @@ app.use(session({
   resave: false,             // Save the session even if it's modified, make this false
   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
 }));
+
+// IMPORT my CONTROLLERS
+// app.use('/factions', require('./controllers/factions'));
 
 app.get('/', function (req, res) {
   res.json({ message: 'Welcome to Subjugate' });
@@ -72,5 +75,29 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
+
+// factions.create({
+//   firstFaction: 'Legions of Tyranny',
+//   secondFaction: 'Eldritch Annihilators',
+//   thirdFaction: 'The Faceless Ones'
+// })
+//   .then(function (createdFactions) {
+//     console.log('New Factions', createdFactions);
+//   })
+//   .catch(function (err) {
+//     console.log('ERROR with adding new Factions', error);
+//   });
+
+factionTraits.create({
+  firstFaction: 'Legions of Tyranny',
+  secondFaction: 'Eldritch Annihilators',
+  thirdFaction: 'The Faceless Ones'
+})
+  .then(function (createdFactions) {
+    console.log('New Factions', createdFactions);
+  })
+  .catch(function (err) {
+    console.log('ERROR with adding new Factions', error);
+  });
 
 module.exports = server;
