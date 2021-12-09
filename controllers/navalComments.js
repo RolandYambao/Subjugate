6761fd549b716cd458ca4923f4bf7ac12b35c56b
RolandYambao <rolandyambao@gmail.com>
2021-12-09 15:08:11 -0800

    Updated

--- controllers/navalComments.js
+++ controllers/navalComments.js
@@ -2,4 +2,61 @@ const express = require('express');
 const router = express.Router();
 const { navalComments } = require("../models");
 
+// Get Routes
+router.get('/', function (req, res) {
+    navalComments.findAll()
+        .then(function (navalCommentList) {
+            res.render('navalComments', { navalComments: navalCommentList });
+        })
+        .catch(function (err) {
+            console.log('ERROR', err);
+            res.json({ message: 'Error occured, please try again....' });
+        });
+});
+
+// Post Routes
+router.post('/', function (req, res) {
+    navalComments.create({
+        content: req.body.content,
+    })
+        .then(function (newNavalComment) {
+            newNavalComment = newNavalComment.toJSON();
+            res.redirect('/navalUnits');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not added, please try again' });
+        });
+});
+
+// Edit
+router.put('/:id', function (req, res) {
+    let navalCommentIndex = Number(req.params.id);
+    navalComments.update({
+        content: req.body.content,
+    }, { where: { id: navalCommentIndex } })
+        .then(function (response) {
+            console.log('AFTER UPDATE', response);
+            res.redirect('/navalUnits');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Update was not successful. Please try again.' });
+        });
+});
+
+// Delete
+router.delete('/:id', function (req, res) {
+    let navalCommentIndex = Number(req.params.id);
+    navalComments.destroy({ where: { id: navalCommentIndex } })
+        .then(function (response) {
+            console.log('COMMENT DELETED', response);
+            res.redirect('/navalUnits');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not deleted, please try again....' });
+        });
+});
+
 module.exports = router;
--- controllers/navalUnits.js
+++ controllers/navalUnits.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { navalUnits } = require("../models");
+const { navalComments } = require("../models");
 
 // Get Route
 router.get('/', function (req, res) {
     navalUnits.findAll()
         .then(function (navalUnitsList) {
-            res.render('navalUnits/index', { navalUnits: navalUnitsList });
+            navalComments.findAll()
+                .then(function (navalCommentsList) {
+                    res.render('navalUnits/index', { navalUnits: navalUnitsList, navalComments: navalCommentsList });
+                })
+                .catch(function (err) {
+                    console.log('ERROR', err);
+                    res.json({ message: 'Error occured, please try again....' });
+                });
         })
         .catch(function (err) {
             console.log('ERROR', err);
