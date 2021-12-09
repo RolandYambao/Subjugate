bfb17ae946681d03f86047f12e99401f9096e751
RolandYambao <rolandyambao@gmail.com>
2021-12-09 14:11:10 -0800

    Updated

--- controllers/defensiveBuildComments.js
+++ controllers/defensiveBuildComments.js
@@ -2,4 +2,61 @@ const express = require('express');
 const router = express.Router();
 const { defensiveBuildComments } = require("../models");
 
+// Get Routes
+router.get('/', function (req, res) {
+    defensiveBuildComments.findAll()
+        .then(function (defensiveBuildCommentsList) {
+            res.render('defensiveBuildComments', { defensiveBuildComments: defensiveBuildCommentsList });
+        })
+        .catch(function (err) {
+            console.log('ERROR', err);
+            res.json({ message: 'Error occured, please try again....' });
+        });
+});
+
+// Post Routes
+router.post('/', function (req, res) {
+    defensiveBuildComments.create({
+        content: req.body.content,
+    })
+        .then(function (newDefensiveBuildComment) {
+            newDefensiveBuildComment = newDefensiveBuildComment.toJSON();
+            res.redirect('/defensiveBuildings');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not added, please try again' });
+        });
+});
+
+// Edit
+router.put('/:id', function (req, res) {
+    let defensiveBuildCommentIndex = Number(req.params.id);
+    defensiveBuildComments.update({
+        content: req.body.content,
+    }, { where: { id: defensiveBuildCommentIndex } })
+        .then(function (response) {
+            console.log('AFTER UPDATE', response);
+            res.redirect('/defensiveBuildings');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Update was not successful. Please try again.' });
+        });
+});
+
+// Delete
+router.delete('/:id', function (req, res) {
+    let defensiveBuildCommentIndex = Number(req.params.id);
+    defensiveBuildComments.destroy({ where: { id: defensiveBuildCommentIndex } })
+        .then(function (response) {
+            console.log('COMMENT DELETED', response);
+            res.redirect('/defensiveBuildings');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not deleted, please try again....' });
+        });
+});
+
 module.exports = router;
--- controllers/defensiveBuildings.js
+++ controllers/defensiveBuildings.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { defensiveBuildings } = require("../models");
+const { defensiveBuildComments } = require("../models");
 
 // Get Route
 router.get('/', function (req, res) {
     defensiveBuildings.findAll()
         .then(function (defensiveBuildingsList) {
-            res.render('defensiveBuildings/index', { defensiveBuildings: defensiveBuildingsList });
+            defensiveBuildComments.findAll()
+                .then(function (defensiveBuildCommentsList) {
+                    res.render('defensiveBuildings/index', { defensiveBuildings: defensiveBuildingsList, defensiveBuildComments: defensiveBuildCommentsList });
+                })
+                .catch(function (err) {
+                    console.log('ERROR', err);
+                    res.json({ message: 'Error occured, please try again....' });
+                });
         })
         .catch(function (err) {
             console.log('ERROR', err);
