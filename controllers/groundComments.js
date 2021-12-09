7c70f0e06110cd1492d60194ed779f9ad423f6d7
RolandYambao <rolandyambao@gmail.com>
2021-12-09 14:47:33 -0800

    Updated

--- controllers/groundComments.js
+++ controllers/groundComments.js
@@ -2,4 +2,61 @@ const express = require('express');
 const router = express.Router();
 const { groundComments } = require("../models");
 
+// Get Routes
+router.get('/', function (req, res) {
+    groundComments.findAll()
+        .then(function (groundCommentsList) {
+            res.render('groundComments', { groundComments: groundCommentsList });
+        })
+        .catch(function (err) {
+            console.log('ERROR', err);
+            res.json({ message: 'Error occured, please try again....' });
+        });
+});
+
+// Post Routes
+router.post('/', function (req, res) {
+    groundComments.create({
+        content: req.body.content,
+    })
+        .then(function (newGroundComment) {
+            newGroundComment = newGroundComment.toJSON();
+            res.redirect('/groundUnits');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not added, please try again' });
+        });
+});
+
+// Edit
+router.put('/:id', function (req, res) {
+    let groundCommentIndex = Number(req.params.id);
+    groundComments.update({
+        content: req.body.content,
+    }, { where: { id: groundCommentIndex } })
+        .then(function (response) {
+            console.log('AFTER UPDATE', response);
+            res.redirect('/groundUnits');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Update was not successful. Please try again.' });
+        });
+});
+
+// Delete
+router.delete('/:id', function (req, res) {
+    let groundCommentIndex = Number(req.params.id);
+    groundComments.destroy({ where: { id: groundCommentIndex } })
+        .then(function (response) {
+            console.log('COMMENT DELETED', response);
+            res.redirect('/groundUnits');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not deleted, please try again....' });
+        });
+});
+
 module.exports = router;
--- controllers/groundUnits.js
+++ controllers/groundUnits.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { groundUnits } = require("../models");
+const { groundComments } = require("../models");
 
 // Get Route
 router.get('/', function (req, res) {
     groundUnits.findAll()
         .then(function (groundUnitsList) {
-            res.render('groundUnits/index', { groundUnits: groundUnitsList });
+            groundComments.findAll()
+                .then(function (groundCommentsList) {
+                    res.render('groundUnits/index', { groundUnits: groundUnitsList, groundComments: groundCommentsList });
+                })
+                .catch(function (err) {
+                    console.log('ERROR', err);
+                    res.json({ message: 'Error occured, please try again....' });
+                });
         })
         .catch(function (err) {
             console.log('ERROR', err);
