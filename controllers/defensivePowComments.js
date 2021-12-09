8256196c50cb824145ac45ba6bfa780e0b7ecda9
RolandYambao <rolandyambao@gmail.com>
2021-12-09 14:28:10 -0800

    Updated

--- controllers/defensivePowComments.js
+++ controllers/defensivePowComments.js
@@ -2,4 +2,61 @@ const express = require('express');
 const router = express.Router();
 const { defensivePowComments } = require("../models");
 
+// Get Routes
+router.get('/', function (req, res) {
+    defensivePowComments.findAll()
+        .then(function (defensivePowCommentsList) {
+            res.render('defensivePowComments', { defensivePowComments: defensivePowCommentsList });
+        })
+        .catch(function (err) {
+            console.log('ERROR', err);
+            res.json({ message: 'Error occured, please try again....' });
+        });
+});
+
+// Post Routes
+router.post('/', function (req, res) {
+    defensivePowComments.create({
+        content: req.body.content,
+    })
+        .then(function (newDefensivePowComment) {
+            newDefensivePowComment = newDefensivePowComment.toJSON();
+            res.redirect('/defensivePowers');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not added, please try again' });
+        });
+});
+
+// Edit
+router.put('/:id', function (req, res) {
+    let defensivePowCommentIndex = Number(req.params.id);
+    defensivePowComments.update({
+        content: req.body.content,
+    }, { where: { id: defensivePowCommentIndex } })
+        .then(function (response) {
+            console.log('AFTER UPDATE', response);
+            res.redirect('/defensivePowers');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Update was not successful. Please try again.' });
+        });
+});
+
+// Delete
+router.delete('/:id', function (req, res) {
+    let defensivePowCommentIndex = Number(req.params.id);
+    defensivePowComments.destroy({ where: { id: defensivePowCommentIndex } })
+        .then(function (response) {
+            console.log('COMMENT DELETED', response);
+            res.redirect('/defensivePowers');
+        })
+        .catch(function (error) {
+            console.log('ERROR', error);
+            res.render('404', { message: 'Comment was not deleted, please try again....' });
+        });
+});
+
 module.exports = router;
--- controllers/defensivePowers.js
+++ controllers/defensivePowers.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { defensivePowers } = require("../models");
+const { defensivePowComments } = require("../models");
 
 // Get Route
 router.get('/', function (req, res) {
     defensivePowers.findAll()
         .then(function (defensivePowersList) {
-            res.render('defensivePowers/index', { defensivePowers: defensivePowersList });
+            defensivePowComments.findAll()
+                .then(function (defensivePowCommentsList) {
+                    res.render('defensivePowers/index', { defensivePowers: defensivePowersList, defensivePowComments: defensivePowCommentsList });
+                })
+                .catch(function (err) {
+                    console.log('ERROR', err);
+                    res.json({ message: 'Error occured, please try again....' });
+                });
         })
         .catch(function (err) {
             console.log('ERROR', err);
