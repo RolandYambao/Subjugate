97591677d3fc46f814ff7c6f15602b605864e5c7
RolandYambao <rolandyambao@gmail.com>
2021-12-10 09:23:00 -0800

    Updated

--- myModule.js
+++ myModule.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function printData() {
     const { data } = await axios.get("https://api.country.is/");
-    console.log(data);
+    console.log(data.country);
 }
 
 module.exports = {
