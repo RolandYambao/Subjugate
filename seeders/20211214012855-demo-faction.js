e67aa354f0d698ae24f223910d11594d8d4a91ea
RolandYambao <rolandyambao@gmail.com>
2021-12-13 17:32:54 -0800

    Updated

---
+++ seeders/20211214012855-demo-faction.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('factions', [
+      {
+        firstFaction: 'Legions of Tyranny',
+        secondFaction: 'Eldritch Annihilators',
+        thirdFaction: 'The Faceless Ones'
+      }
+    ])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
